ee067d70da07e401bb454cea9a7038c6907e5a71
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-18 11:01:59 -0800

    Follow along Guided project.

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,24 +1,38 @@
-const bcrypt = require('bcryptjs');
+// const bcrypt = require('bcryptjs');
 
-const Users = require('../users/users-model.js');
+// const Users = require('../users/users-model.js');
 
 module.exports = (req, res, next) => {
-  const { username, password } = req.headers;
+	const keyToken = req.headers;
+	if (keyToken) {
+		res.status(400).json({ message: 'There is no Token' });
+	} else {
+		let correctToken = jwt.verify(token, 'is it secret, is it safe?');
 
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json({ message: 'Ran into an unexpected error' });
-      });
-  } else {
-    res.status(400).json({ message: 'No credentials provided' });
-  }
+		if (correctToken) {
+			next();
+		} else {
+			res.status(500).json({ message: 'There was an error with that token!' });
+		}
+	}
 };
+
+	// const { username, password } = req.headers;
+
+	// if (username && password) {
+	//   Users.findBy({ username })
+	//     .first()
+	//     .then(user => {
+	//       if (user && bcrypt.compareSync(password, user.password)) {
+	//         next();
+	//       } else {
+	//         res.status(401).json({ message: 'Invalid Credentials' });
+	//       }
+	//     })
+	//     .catch(error => {
+	//       res.status(500).json({ message: 'Ran into an unexpected error' });
+	//     });
+	// } else {
+	//   res.status(400).json({ message: 'No credentials provided' });
+	// }
+
